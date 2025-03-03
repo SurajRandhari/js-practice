d52ad6f0c92b1b6892eea48f9b9cad4558fd8c83
Suraj Randhari <110189854+SurajRandhari@users.noreply.github.com>
2025-03-03 16:44:22 +0000

    array method

--- 01-basic/test.js
+++ 01-basic/test.js
@@ -1,1 +1,18 @@
 console.log("Suraj")
+
+arr = [2,3,4,5,6,7,8]
+
+// const gg = arr.map((n) => n*2)
+// console.log(gg);
+
+// const ff = arr.filter((n) => {
+//     if (n >= 6) {
+//      return   n
+//     }
+// })
+// console.log(ff);
+
+
+// const redu = arr.reduce((acc, cur)=> acc + cur  )
+// console.log(redu);
+
