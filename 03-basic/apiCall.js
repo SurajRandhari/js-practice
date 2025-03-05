98abd674ddd927eb72bec55206f7fc1791115dee
Suraj Randhari <110189854+SurajRandhari@users.noreply.github.com>
2025-03-05 17:48:07 +0000

    api calls -001

--- 03-basic/apiCall.js
+++ 03-basic/apiCall.js
@@ -1,7 +1,25 @@
 
-const res = fetch('https://dummyjson.com/users')
-            .then(response => response.json())
-            .then(data => console.log(data));
+// const res = fetch('https://dummyjson.com/users')
+//             .then(response => response.json())
+//             .then(data => console.log(data));
             
 
+
+            async function fetchUsers() {
+                try {
+                    const response = await fetch('https://dummyjson.com/users');
+                    
+                    if (!response.ok) {
+                        throw new Error('Network response was not ok');
+                    }
+                    
+                    const data = await response.json();
+                    console.log(data);
+                } catch (error) {
+                    console.error('There was a problem with the fetch operation:', error);
+                }
+            }
+            
+            // Call the function to fetch users
+            fetchUsers();
 
